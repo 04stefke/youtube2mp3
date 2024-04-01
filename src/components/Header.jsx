fccf2033608f0f8741d3259b67333492f735619e
04stefke <jovanovics399@yahoo.com>
2024-04-01 11:45:34 +0100

    global styles and logo inside the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import Logo from '../assets/logo.webp'
+import '../styles/Header.css'
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='header-component'>
+        <div className="logo">
+            <img src={Logo} alt="logo" className='logo' />
+        </div>
+        <div className="app-title">Youtube Converter</div>
+    </div>
   )
 }
 
