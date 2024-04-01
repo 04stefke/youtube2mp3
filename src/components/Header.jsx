d0eebaee8328ce46ba02b34d4800dbb85b157b33
04stefke <jovanovics399@yahoo.com>
2024-04-01 11:54:51 +0100

    created the header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import Logo from '../assets/logo.webp'
-import '../styles/Header.css'
+import React from "react";
+import Logo from "../assets/logo.webp";
+import "../styles/Header.css";
 const Header = () => {
-  return (
-    <div className='header-component'>
-        <div className="logo">
-            <img src={Logo} alt="logo" className='logo' />
-        </div>
-        <div className="app-title">Youtube Converter</div>
-    </div>
-  )
-}
+	return (
+		<div className="header-component">
+			<div className="logo">
+				<img src={Logo} alt="logo" className="logo" />
+			</div>
+			<div className="app-title">
+				<h1>Youtube Converter</h1>
+			</div>
+		</div>
+	);
+};
 
-export default Header
+export default Header;
